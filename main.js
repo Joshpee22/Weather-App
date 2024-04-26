03cb0637724fed1cfc9c262294dcc51733d34bf8
joshpee22 <owobujoshua2017@gmail.com>
2024-04-26 05:11:14 -0700

    updated Api

---
+++ main.js
@@ -0,0 +1,18 @@
+const apiKey = "fa751b1de739d10f650b6739ee1f51b7";
+const city = "Abuja"; // Replace with the name of the city you want weather data for
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+async function checkWeather() {
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
+}
+
+checkWeather()
